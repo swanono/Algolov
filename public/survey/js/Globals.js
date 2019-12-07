a86292eb790916bbde7bbe62b24e4c718ab76873
Sacha WANONO <sacha.wanono@etu.univ-nantes.fr>
2019-12-07 18:05:35 +0100

    Feature/intro quest - ajout de l'avancement

--- public/survey/js/Globals.js
+++ public/survey/js/Globals.js
@@ -18,15 +18,57 @@ along with this program. If not, see < https://www.gnu.org/licenses/ >.
 This module is used to declare global variables and functions
 */
 
+/* global TraceStorage */
+/* global DOMGenerator */
+
 'use strict';
 
 window.state = 0;
-window.config = {}; // TODO : charger config.json dans cet objet
-
-function changeState () {
-
-}
+window.config = {}; // Contains the config.json file
+window.features = null; // Contains all the features
 
 function start () {
+    // Start the questionnaire, to use at the first
 
+    // Get the config.json file
+    fetch('../config.json', { method: 'GET' })
+        .then(res => res.json())
+        .then(function (data) {
+            window.config = data;
+            loadFeatures();
+            changeState();
+        })
+        .catch(e => console.log(e));
+}
+
+function loadFeatures () {
+    TraceStorage.CleanStorageFormTraces();
+
+    if (window.config.features) {
+        window.features = window.config.features;
+    } else {
+        alert(window.config.wrongstatementformatmessage);
+    }
+}
+
+function changeState () {
+    window.state++; // Update the state
+    switch (window.state) {
+    case 1 :
+        DOMGenerator.GenerateStepPage(window.config.RGPDText, 'Démarrer', () => changeState());
+        DOMGenerator.addCheckBoxToSee('button', 'Acceptez-vous les conditions ci-dessus ? ');
+        break;
+
+    case 2 :
+        DOMGenerator.GenerateStepPage(window.config.surveyExplain,
+            'Continuez',
+            () => changeState());
+
+        break;
+
+    default :
+        console.log(window.state);
+        console.log('This state doesn\'t exist');
+        break;
+    }
 }
