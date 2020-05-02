54219a513c59eb1982e5d001c11b7eaae89cd23c
Sacha Wanono <sacha.wanono@etu.univ-nantes.fr>
2020-05-02 04:03:13 +0200

    [CODE] Fonction d'ajout question begin

--- QuestionsReader.js
+++ QuestionsReader.js
@@ -31,7 +31,8 @@ function isString (v) {
 class QuestionsReader extends ExcelReader {
     constructor (filePath) {
         super(filePath, 'question');
-        this.questSheet = this.workbook.Sheets[config.excelSheetNames.questSheet];
+        this.questBeginSheet = this.workbook.Sheets[config.excelSheetNames.questBeginSheet];
+        this.questEndSheet = this.workbook.Sheets[config.excelSheetNames.questEndSheet];
         this.textSheet = this.workbook.Sheets[config.excelSheetNames.textSheet];
         this.introSheet = this.workbook.Sheets[config.excelSheetNames.introSheet];
         this._init();
@@ -40,7 +41,8 @@ class QuestionsReader extends ExcelReader {
     _init () {
         
         const oldConfig = JSON.parse(fs.readFileSync('./public/survey/documents/config.json'));
-
+        
+        /*
         // Retrieving description datas
         let range = XLSX.utils.decode_range(this.questSheet['!ref']);
 
@@ -130,10 +132,12 @@ class QuestionsReader extends ExcelReader {
 
             this.newConfig.questions.push(question);
         }
+        */
 
+        this.importQuestionsDatas('questBeginSheet', false);
+        this.importQuestionsDatas('questEndSheet', true);
 
-
-        range = XLSX.utils.decode_range(this.textSheet['!ref']);
+        let range = XLSX.utils.decode_range(this.textSheet['!ref']);
 
 
         this.newConfig.textButton = {};
@@ -150,7 +154,8 @@ class QuestionsReader extends ExcelReader {
         this.newConfig.RGPDText = this.introSheet[XLSX.utils.encode_cell({ r: 0, c: 1 })].v.trim();
         this.newConfig.RGPDValidation = this.introSheet[XLSX.utils.encode_cell({ r: 1, c: 1 })].v.trim();
         this.newConfig.surveyExplain = this.introSheet[XLSX.utils.encode_cell({ r: 2, c: 1 })].v.trim();
-
+        
+        /*
         // Updating information about related question
         
         for (let indexQuest = 0; indexQuest < relatedQuestions.length; indexQuest++) {
@@ -184,7 +189,7 @@ class QuestionsReader extends ExcelReader {
 
             }
         }
-        
+        */
         
 
     }
@@ -216,7 +221,7 @@ class QuestionsReader extends ExcelReader {
     validate () {
 
 
-        // TODO : check les string de textbutton
+        // Check les string de textbutton
 
         const boolContinue = !(isString(this.newConfig.textButton.continue) && this.newConfig.textButton.continue !== '');
         const boolConfirm = !(isString(this.newConfig.textButton.confirm) && this.newConfig.textButton.confirm !== '');
@@ -225,10 +230,11 @@ class QuestionsReader extends ExcelReader {
         if (boolContinue || boolConfirm || boolStopSurvey || boolStartSurvey)
             this.xlsErrors.push('Un des textes n\'a pas été défini');
 
-        // TODO : check que les question de related question exist (et que les choix existe?)
 
+        // Check que les question de related question exist (et que les choix existe?)
 
-
+        // TODO: adapter le check au question Begin et end
+        /*
         this.newConfig.questions.forEach((quest, i) => {
             if (!(isString(quest.question)))
                 this.xlsErrors.push('La description n°' + i + ' n\'a pas été défini');
@@ -253,7 +259,7 @@ class QuestionsReader extends ExcelReader {
                     
                 });
             }
-        });
+        });*/
 
 
         return this.xlsErrors;
@@ -272,6 +278,149 @@ class QuestionsReader extends ExcelReader {
         oldConfig.QCM.end.list = this.newConfig.questions;
 
         fs.writeFileSync('./public/survey/documents/config.json', JSON.stringify(oldConfig, null, 4));
+    }
+
+
+
+
+    importQuestionsDatas (questSheet, isEnd) {
+
+        const oldConfig = JSON.parse(fs.readFileSync('./public/survey/documents/config.json'));
+        let range = XLSX.utils.decode_range(this[questSheet]['!ref']);
+
+
+        let questionsList = [];
+        let relatedQuestions = [];
+        
+        let numLastBeginQuest = oldConfig.QCM.begin.list.length;
+
+        if (isEnd)
+            numLastBeginQuest = this.newConfig.questBegin.length + 2;
+        else
+            numLastBeginQuest = 2;
+        
+        for (let row = range.s.r + 1; row <= range.e.r; row++) {
+            const validTypes = ExcelReader._detectInvalidTypes(this[questSheet], row, [
+                {num: 0, functorBool: isString},
+                {num: 1, functorBool: isString},
+                {num: 2, functorBool: isString}
+            ]);
+            if (!validTypes)
+                continue;
+
+            
+
+            const question = {};
+
+            question.id = numLastBeginQuest + row - range.s.r;
+            question.question = this[questSheet][XLSX.utils.encode_cell({ r: row, c: 0 })].v.trim(); 
+            const type = this[questSheet][XLSX.utils.encode_cell({ r: row, c: 1 })].v.trim().split(',');
+
+            if (type[0].toLowerCase() === 'date') {
+                question.type = 'text';
+                question.format = '^(((0[1-9])|(1[0-9]))\\/((19[89][0-9])|(20[0-9]{2})))$';
+            } else if (['email','number','checkbox','radio','text','longcheckbox','longradio'].includes(type[0].toLowerCase()))
+                question.type = type[0].toLowerCase();
+            else if (type[0] === 'age') {
+                question.type = 'text';
+                question.format = '^([0-9]{2,3})$';
+            } else if (type[0] === 'tel') {
+                question.type = 'tel';
+                question.format = '^((\\+\\d{1,3}(-| )?\\(?\\d\\)?(-| )?\\d{1,5})|(\\(?\\d{2,6}\\)?))(-| )?(\\d{3,4})(-| )?(\\d{4})(( x| ext)\\d{1,5}){0,1}$';
+            } else {
+                console.error('Type de question non accepté à la ligne ' + row);
+                break;
+            }
+            if (type.length > 1 )
+                question.other = true;
+            
+            if (this[questSheet][XLSX.utils.encode_cell({ r: row, c: 2 })].v !== 0){
+                // Array of information for question and answer necessary to access at this question
+                
+                // Managing the relation between column name and answers id
+                const combin = this[questSheet][XLSX.utils.encode_cell({ r: row, c: 2 })].v.trim().split(',');
+                let necessaryAnswersTab = [];
+                for (let i = 1; i < combin.length ; i++) 
+                    necessaryAnswersTab.push(combin[i].trim().toLowerCase().charCodeAt(0) - 'd'.charCodeAt(0) + 1);
+                
+
+                relatedQuestions.push( {
+                    conditionnalQuest: question.id,
+                    necessaryQuestion: numLastBeginQuest + parseInt(combin[0].trim()) - 1 - range.s.r, 
+                    necessaryAnswers : necessaryAnswersTab
+                });
+                
+
+                
+                
+            }
+
+
+            if (question.type == 'checkbox' || question.type == 'longcheckbox' || question.type == 'radio' || question.type == 'longradio') {
+                question.choices = [];
+                for (let col = 3; col <= range.e.c; col++) {
+                    // TODO: Voir si il faut pas adapter ??? 
+                    const validTypes2 = ExcelReader._detectInvalidTypes(this[questSheet], 0, [
+                        {num: col, functorBool: isString}
+                    ]) && ExcelReader._detectInvalidTypes(this[questSheet], row, [
+                        {num: col, functorBool: isString}
+                    ]);
+                    if (!validTypes2)
+                        continue;
+                    
+                    const newChoice = {};
+                    newChoice.choiceId = col - 2; // col start at 3
+                    if (this[questSheet][XLSX.utils.encode_cell({ r: row, c: col })]) {
+                        newChoice.text = this[questSheet][XLSX.utils.encode_cell({ r: row, c: col })].v.trim();
+                        question.choices.push(newChoice);
+                    }
+                    
+                }
+            }
+
+
+            questionsList.push(question);
+        }
+
+        // Updating information about related question
+        for (let indexQuest = 0; indexQuest < relatedQuestions.length; indexQuest++) {
+            const idQuest = relatedQuestions[indexQuest].necessaryQuestion; 
+            
+            const question = questionsList.find( quest => quest.id === idQuest);
+            
+            if (question) {
+                const choices = Array.from(question.choices || {}, choice => choice.choiceId);
+                const relQanswers = choices.filter(elem => !(relatedQuestions[indexQuest].necessaryAnswers).includes(elem));
+                
+                if (!question.relatedQuestion)
+                    question.relatedQuestion = [];
+
+                let relQuest = question.relatedQuestion.find( question => {
+                    let boolEqualAr = true;
+                    relQanswers.forEach(elem => {
+                        if (!question.triggerChoices.includes(elem))
+                            boolEqualAr = false;
+                    });
+                    return boolEqualAr;});
+
+                
+                if (!relQuest) {
+                    question.relatedQuestion.push({
+                        "triggerChoices": relQanswers, //pas bon
+                        "questionIds": [relatedQuestions[indexQuest].conditionnalQuest]
+                    });
+                } else if (!relQuest.questionIds.includes(idQuest))
+                    relQuest.questionIds.push(relatedQuestions[indexQuest].conditionnalQuest);
+
+            }
+        }
+
+        // Set the prop of the newConfig question list (Begin or End)
+        if (isEnd)
+            this.newConfig.questEnd = questionsList;
+        else 
+            this.newConfig.questBegin = oldConfig.QCM.begin.list.slice(0,2).concat(questionsList);
+
     }
 
 }
